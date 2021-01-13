78c6bbc9cbf05c65df5dbde3a0f5ade25764870a
Peiran Yu <pyu@cs.ualberta.ca>
2021-01-13 09:38:10 -0700

    second step

--- lab1.py
+++ lab1.py
@@ -3,6 +3,9 @@ import requests
 print(requests.__version__)
 
 url1 = "http://google.com"
-r1 = requests.get(url1)
+r1 = requests.get("http://google.com")
 print(r1.status_code)
-print(r1.text)
+print(r1.text.encode('utf-8'))
+
+rawUrl = requests.get("https://raw.githubusercontent.com/TaraYu/cmput404lab1/master/lab1.py")
+print(rawUrl.text.encode('utf-8'))
